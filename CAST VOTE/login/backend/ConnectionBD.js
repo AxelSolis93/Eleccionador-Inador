d32f041892eb1b27017f616c3b853e0926df649e
JoelDiaz0 <95628128+JoelDiaz0@users.noreply.github.com>
2023-08-07 04:02:34 -0500

    07/08

---
+++ CAST VOTE/login/backend/ConnectionBD.js
@@ -0,0 +1,15 @@
+
+
+export class ConnectionBD {
+    host;
+    user;
+    password;
+    database;
+    conn;
+
+    constructor() {
+        throw Error('No se puede inicialiar un clase abstracta!');
+    }
+
+    
+}
